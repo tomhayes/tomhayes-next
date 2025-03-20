bfcb5852125024d73d510593ecd5b60d2b73fb15
Tom Hayes <tom@theideabureau.co>
2025-03-20 00:06:26 +0000

    add basic contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,23 @@
+import { Section, Container, Prose } from "@/components/craft";
+import { Metadata } from "next";
+import BackButton from "@/components/back";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Contact",
+  description: "Get in touch with me."
+};
+
+export default function Contact() {
+  return (
+    <Section>
+      <Container className="space-y-6">
+        <Prose className="mb-8">
+          <h2>Get in touch</h2>
+            <p>You can reach me at <a href="mailto:hello@tomhay.es">hello@tomhay.es</a>.</p>
+        </Prose>
+        <BackButton />
+      </Container>
+    </Section>
+  );
+}
